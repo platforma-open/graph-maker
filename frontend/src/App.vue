da89b9a7fe0ca21bb297f6533f455b1831cca3a5
Elena Erokhina <erohinaelena1@yandex.ru>
2024-08-07 14:49:32 +0300

    add charts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import '@milaboratory/platforma-uikit/dist/style.css';
+import '@milaboratory/graph-maker/dist/style.css';
 import { computed, reactive, watch } from 'vue';
 import { PColumnIdAndSpec } from '@milaboratory/sdk-ui';
 import { BlockApp } from './app';
 import { platforma } from 'block-config';
+import { GraphMakerSettings } from '@milaboratory/graph-maker/dist/GraphMaker/types';
+import { GraphMaker } from '@milaboratory/graph-maker';
+import {AddGraph} from '@milaboratory/platforma-uikit';
 
 const app = BlockApp.use();
 
@@ -12,25 +16,61 @@ const data = reactive({
 });
 
 const frameRef = computed(() => app.getOutputFieldOkOptional('pFrame'));
+
 watch(() => frameRef.value, async (frameHandle) => {
   data.pFrameSpecs = undefined;
   if (frameHandle) {
     data.pFrameSpecs = await platforma.pFrameDriver.listColumns(frameHandle);
   }
-}, { immediate: true })
+}, { immediate: true });
 
+const settings = reactive<Partial<GraphMakerSettings>>({
+  optionsState: null,
+  statisticsSettings: null,
+  axesSettings: null,
+  layersSettings: null,
+  template: null,
+  dataBindAes: null,
+  labelsModifier: (v) => v
+});
+
+function onSettingsUpdate(nextSettings: GraphMakerSettings) {
+  console.log(nextSettings, 'next settings');
+  settings.optionsState = nextSettings.optionsState;
+  settings.statisticsSettings = nextSettings.statisticsSettings;
+  settings.axesSettings = nextSettings.axesSettings;
+  settings.template = nextSettings.template;
+  settings.dataBindAes = nextSettings.dataBindAes;
+}
+
+const CHART_TYPES = [{group: 'Charts', items: [
+    {image: 'discrete', title: 'Discrete', id: 'discrete'},
+    {image: 'scatterplot', title: 'Scatter Plot', id: 'scatterplot'},
+    {image: 'heatmap', title: 'Heatmap', id: 'heatmap'}
+  ]}]
 </script>
 
 <template>
   <div class="container">
-    <h3>Graphmaker test</h3>
-
-    <fieldset>
-      <legend>Frame Specs</legend>
-      <pre>{{ data.pFrameSpecs }}</pre>
-    </fieldset>
+    <graph-maker
+      v-if="platforma.pFrameDriver && frameRef && settings.chartType"
+      :key="settings.chartType"
+      :settings="settings as GraphMakerSettings"
+      :p-frame-handle="frameRef"
+      :p-frame-driver="platforma.pFrameDriver"
+      @settings-update="onSettingsUpdate"
+    />
+    <add-graph
+      v-if="!settings.chartType"
+      @selected="(v) => settings.chartType = v as GraphMakerSettings['chartType']"
+      :items="CHART_TYPES"
+    />
 
   </div>
+    <fieldset>
+    <legend>Frame Specs</legend>
+    <pre>{{ data.pFrameSpecs }}</pre>
+  </fieldset>
 </template>
 
 <style lang="css">
@@ -44,6 +84,7 @@ watch(() => frameRef.value, async (frameHandle) => {
   display: flex;
   flex-direction: column;
   gap: 24px;
+  height: 1080px;
 }
 
 fieldset {

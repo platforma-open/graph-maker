b615226d4ffd5ef70031135974247ef56f814add
gramkin <bakluzhino@gmail.com>
2024-11-16 21:09:53 +0100

    Update ui-vue & graph-maker, fix minor style issues

---
+++ .changeset/chilly-cobras-melt.md
@@ -0,0 +1,6 @@
+---
+'@platforma-open/milaboratories.graph-maker.ui': patch
+'@platforma-open/milaboratories.graph-maker': patch
+---
+
+Update ui-vue & graph-maker, fix minor style issues
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue';
 import { sdkPlugin } from './app';
-import '@milaboratories/uikit/dist/style.css';
-import '@milaboratories/graph-maker/dist/style.css';
-import '@platforma-sdk/ui-vue/dist/style.css';
+import '@platforma-sdk/ui-vue/styles';
+import '@milaboratories/graph-maker/styles';
 import { BlockLayout } from '@platforma-sdk/ui-vue';
 
 createApp(BlockLayout).use(sdkPlugin).mount('#app');

9db2a6ca0458f030663cf4898ad82dd948b6db47
Elena Erokhina <erohinaelena1@yandex.ru>
2025-02-19 20:01:04 +0200

    remove temp

--- ui/src/GraphPage.vue
+++ ui/src/GraphPage.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { computed } from 'vue';
 import { useApp } from './app';
 import { GraphMakerState, GraphMaker } from '@milaboratories/graph-maker';
 import { GraphPageState } from '@platforma-open/milaboratories.graph-maker.model';
-import {PlDropdown} from '@platforma-sdk/ui-vue';
 
 const app = useApp<`/graph?id=${string}`>();
 
@@ -48,16 +47,6 @@ const removeSection = async () => {
   }
 };
 
-const dropdownValues = [{value: 'v1', text: 'v1'}, {value: 'v2', text: 'v2'}]
-const dropdownSelected = ref('v1');
-const keyValue = ref('v1');
-
-
-watch(() => dropdownSelected.value, () => {
-  ui.model.graphs = [{ id: 'id', label: 'test label', state: { template: 'box', title: 'tst', currentTab: null }, settings: { chartType: 'discrete', pFrame: undefined } }]
-  keyValue.value = dropdownSelected.value;
-})
-//
 // watch(() => app.model.outputs.pFrame, async (handle) => {
 //   const list = await platforma?.pFrameDriver.listColumns(handle!);
 //   console.log(list, 'list')
@@ -67,19 +56,13 @@ watch(() => dropdownSelected.value, () => {
 <template>
   <div class="container_graph_page" :key="app.queryParams.id">
     <graph-maker
-      :key="keyValue"
       v-if="state && graphProps"
       v-model="state"
       :pFrame="app.model.outputs.pFrame"
       :chartType="graphProps.chartType"
       :allowDeleting="true"
       @delete-this-graph="removeSection"
-    >
-      <template #titleLineSlot>
-        <PlDropdown v-model="dropdownSelected" :options="dropdownValues"
-                    label="Test" :style="{ width: '300px' }" />
-      </template>
-    </graph-maker>
+    />
   </div>
 </template>
 

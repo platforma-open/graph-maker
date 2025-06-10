b5505eefbd884e20ab41eb295363a6330a8ff25b
Elena Erokhina <erohinaelena1@yandex.ru>
2025-06-10 19:49:08 +0300

    update graph-maker version, fix createPFrameForGraphs using

--- model/src/index.ts
+++ model/src/index.ts
@@ -1,7 +1,10 @@
 import {
-  BlockModel, createPFrameForGraphs,
+  AxisId,
+  BlockModel, CanonicalizedJson, canonicalizeJson,
+  enrichCompatible,
+  getAxisId,
   type InferOutputsType,
-  isPColumn, RenderCtx, ValueType
+  isPColumn, PColumn, PColumnDataUniversal, PFrameHandle, RenderCtx, TreeNodeAccessor, ValueType
 } from '@platforma-sdk/model';
 import {GraphMakerProps, GraphMakerState} from '@milaboratories/graph-maker';
 
@@ -17,6 +20,36 @@ export type UiState = {
 };
 
 export type BlockArgs = {};
+
+export function createPFrameForGraphs<A, U>(
+  ctx: RenderCtx<A, U>,
+  allColumns: PColumn<PColumnDataUniversal>[] | undefined,
+): PFrameHandle | undefined {
+  if (!allColumns) return undefined;
+
+  // all possible axes from block columns
+  const allAxes = new Map<CanonicalizedJson<AxisId>, AxisId>();
+  for (const c of allColumns) {
+    for (const id of c.spec.axesSpec) {
+      const aid = getAxisId(id);
+      allAxes.set(canonicalizeJson(aid), aid);
+    }
+  }
+
+  // additional columns are duplicates with extra fields in domains for compatibility if there are ones with partial match
+  const extendedColumns = enrichCompatible(allAxes, allColumns);
+
+  // if at least one column is not yet ready, we can't show the table
+  if (
+    extendedColumns.some(
+      (a) => a.data instanceof TreeNodeAccessor && !a.data.getIsReadyOrError(),
+    )
+  )
+    return undefined;
+
+  return ctx.createPFrame(extendedColumns);
+}
+
 
 export const platforma = BlockModel.create('Heavy')
   .withUiState<UiState>({graphs: []})
